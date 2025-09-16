cb685dfcf08d7a135c904561622ab1b2f5d95cb0
Kim Richards <richards.kimc@gmail.com>
2025-09-16 07:52:33 -0400

    App now dispays a list of articles, with the option to add/delete an article

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,90 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+const initialArticles:any = [{title:'ONE'},{title:'TWO'},{title:'THREE'}]
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  // For storing the article data
+  const [articles, setArticles] = useState(initialArticles);
+  // For storing the selected article id
+  const [selectedArticleId, setSelectedArticleId] = useState(-1);
+
+  // Initial form object to intitialise the forObject state variable
+  const initialFormObject: any = {title: 'title1', content: 'content1'};
+  // For storing the form data
+  const [formObject, setFormObject] = useState(initialFormObject);
+
+  // Function to retrieve the articles from articles.json over the network
+  const getArticles = function(){
+    fetch('articles.json')
+      .then(response => response.json())
+      .then( data => {
+        setArticles(data)
+      });
+  };
+
+  // Use Effect Statement
+  useEffect(() => {getArticles()}, []); // Empty square brackets ensures the code is only called once - after first render
+
+  // Get the selected articles content, or none if no article is selected
+  const selectedArticle = (articles[selectedArticleId]) ? articles[selectedArticleId].content : 'none';
+
+  // Get a new value for the field being changed andassign it to the property of the same name in formObject
+  const changeHandler = function(event: any) {
+    const name = event.target.name;
+    const value = event.target.value;
+    formObject[name] = value;
+    setFormObject({...formObject});
+  }
+
+  // Function to disable/enable delete button, returns true if the selected article is greater than -1 (-1 was the default)
+  const validSelectedArticleId = function() {
+    return (selectedArticleId >= 0 && selectedArticleId < articles.length);
+  }
+
+  // Function to delete the selected article
+  const deleteSelected = function () {
+    if (validSelectedArticleId()) {
+      articles.splice(selectedArticleId, 1);
+      setArticles([...articles]);
+  }
+  }
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={'app'}>
+      <h2>React Hooks App</h2>
+
+      {/* Display a list of articles, as links, that were retrieved from articles.json */}
+      <ul>
+        {articles.map((article: any, index: number) => (
+          // When the list item is clicked, it will apply the 'selected' css styling
+          <li key={index} className={(selectedArticleId == index) ? 'selected' : ''} onClick={(_) => setSelectedArticleId(index)}>
+            {article.title}
+          </li>
+        ))}
+      </ul>
+
+      {/* Display the selected article content */}
+      <br/><span className={'bold'}>Selected Article: </span>
+      <p>{selectedArticle}</p>
+      
+      <div className={'controls'}>
+        <span className={'bold'}>Controls</span><br/>
+        <button onClick={() => setArticles([...articles, formObject])}>Add Article</button>&nbsp;
+        <button disabled={!validSelectedArticleId()} onClick={() => deleteSelected()}>Delete Selected</button>
+        <br />
+        <input type={'text'} name={'title'} placeholder={'title'} value={formObject.title} onChange= {(e)=>changeHandler(e)}/>
+        <br />
+        <input type={'text'} name={'content'} placeholder={'content'} value={formObject.content} onChange= {(e)=>changeHandler(e)}/>
+        <br />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+    </div>
+  );
+
 }
 
 export default App
